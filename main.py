04593a32eb31fd28945ec4a179fa1b75837fc8f2
kev <yqlbu@bu.edu>
2023-12-18 18:08:32 +0800

    refactor: deprecate decorator usage

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import structlog
-import functions_framework
 from cloudevents.http.event import CloudEvent
 
 
-@functions_framework.cloud_event
-def handler(event: CloudEvent) -> None:
+def handler(event: CloudEvent):
     logger = structlog.get_logger()
     try:
         logger.debug("Function loaded")
         logger.debug(f"Received event with ID: {event['id']} and data {event.data}")
         logger.debug(f"Cloudevent: {event}")
         logger.info("OK")
+
+        return "OK", 200
     except Exception as err:
         logger.error(str(err))
+        return "Internal Server Error", 500
